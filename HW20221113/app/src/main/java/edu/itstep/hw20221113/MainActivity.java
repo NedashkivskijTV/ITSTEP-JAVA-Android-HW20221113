5ce2aca22b8215eb3bff62b7a81bc4accf30ec8c
NedashkivskijTV <nedashkivskijtv@ukr.net>
2022-11-15 15:05:31 +0200

    add homework from 13/11/2022

---
+++ HW20221113/app/src/main/java/edu/itstep/hw20221113/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.itstep.hw20221113;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
